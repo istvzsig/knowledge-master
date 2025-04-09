c8fec6e5d2ea8308f9972f1c5f57a86825539e2c
istvzsig <istvanzsigmond@aol.com>
2025-04-09 13:13:59 +0200

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv, { process } from "dotenv";
-import path from "path";
 
-dotenv.config({ path: path.resolve(path.__filename, ".env") });
-
-const BACKEND_URL = process.env.VITE_BACKEND_URL;
-const BACKEND_PORT = process.env.VITE_BACKEND_PORT;
+const BACKEND_URL = process.env.BACKEND_URL || "http://127.0.0.1";
+const BACKEND_PORT = process.env.BACKEND_PORT || 5555;
+const FRONTEND_PORT = process.env.FRONTEND_PORT || 5173;
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.VITE_FRONTEND_PORT,
+    port: FRONTEND_PORT,
     proxy: {
       "/faqs": {
         target: `${BACKEND_URL}:${BACKEND_PORT}`,
