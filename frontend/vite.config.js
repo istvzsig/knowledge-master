10c4977d6d84308d0cac290e19160d1a86133d9d
istvzsig <istvanzsigmond@aol.com>
2025-04-09 12:18:53 +0200

    config vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv, { process } from "dotenv";
+import path from "path";
 
-// https://vite.dev/config/
+dotenv.config({ path: path.resolve(path.__filename, ".env") });
+
+const BACKEND_URL = process.env.VITE_BACKEND_URL;
+const BACKEND_PORT = process.env.VITE_BACKEND_PORT;
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.VITE_FRONTEND_PORT,
     proxy: {
       "/faqs": {
-        target: "http://127.0.0.1:8080",
+        target: `${BACKEND_URL}:${BACKEND_PORT}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/faqs/, "/faqs"),
       },
