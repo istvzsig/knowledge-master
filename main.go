770535c5537459410046398643d1a6b7b7a502b4
istvzsig <istvanzsigmond@aol.com>
2025-04-09 13:25:53 +0200

    fix port in main go

--- main.go
+++ main.go
@@ -4,22 +4,16 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
-
 	"github.com/istvzsig/knowledge-menager/config"
 	"github.com/istvzsig/knowledge-menager/routes"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
 	config.InitFirestore()
 
 	r := routes.SetupRouter()
 
-	port := os.Getenv("PORT")
+	port := os.Getenv("BACKEND_PORT")
 	if port == "" {
 		port = "8080"
 	}
