3f95a6a3e7d46c0198282bb4d4f9732ff05b43d9
istvzsig <istvanzsigmond@aol.com>
2025-04-13 10:40:20 +0200

    delete func from logger

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -2,18 +2,10 @@ package utils
 
 import (
 	"log"
-
-	"github.com/gin-gonic/gin"
 )
 
 func LogError(err error) {
 	if err != nil {
 		log.Println(err)
 	}
-}
-
-// HandleError sends a JSON response with the error message and status code
-func HandleError(c *gin.Context, status int, err any) {
-	log.Printf("Error: %v", err)
-	c.JSON(status, gin.H{"error": err})
 }
